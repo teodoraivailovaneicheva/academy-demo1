154eba1d08d1b15ac41d89e8d236fb8974c56f5a
Teodora Neicheva <teodoraivailovaneicheva@gmail.com>
2023-01-30 20:40:46 +0200

    getters and setters add

--- src/main/java/org/example/model/SportCar.java
+++ src/main/java/org/example/model/SportCar.java
@@ -4,5 +4,12 @@ public class SportCar extends Car{
 
     private int spoiler;
 
+    public int getSpoiler() {
+        return spoiler;
+    }
+
+    public void setSpoiler(int spoiler) {
+        this.spoiler = spoiler;
+    }
 
 }
